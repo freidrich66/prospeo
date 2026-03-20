458309ed31a07b26890affb8517675ec0f6ebf31
freidrich66 <freidrich@hotmail.fr>
2026-03-20 16:57:04 +0100

    Fix import statement typo in supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
-iimport { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey  = import.meta.env.VITE_SUPABASE_ANON_KEY
@@ -8,3 +8,4 @@ if (!supabaseUrl || !supabaseKey) {
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
